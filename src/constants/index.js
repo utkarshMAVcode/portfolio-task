180c0c9e4c49c798408b7715d39201e9f50406ff
utkarshMAVcode <utkarsh.mission@gmail.com>
2023-06-21 15:07:03 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,271 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Front-End Developer',
+    company_name: 'Cover Hunt',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Aug 2021 - Feb 2022',
+  },
+  {
+    title: 'Mentor (Volunteer)',
+    company_name: 'Microverse',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Mar 2022 - May 2022',
+  },
+  {
+    title: 'Junior Software Engineer',
+    company_name: 'Kelhel',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'May 2022 - Oct 2022',
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Diversity Cyber Council',
+    icon: dcc,
+    iconBg: '#333333',
+    date: 'Sep 2022 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'calculator',
+    description: 'An online calculator application.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/utkarshMAVcode/mern-react-task-2',
+    demo: 'https://mern-react-task-2-au8y.vercel.app/',
+  },
+  {
+    id: 'project-2',
+    name: 'color picker',
+    description:
+      'An online application that lets us pick different color for the back ground',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/utkarshMAVcode/mern-react-task-3',
+    demo: 'https://mern-react-task-3.vercel.app/',
+  },
+  {
+    id: 'project-3',
+    name: 'age calculator',
+    description: 'an online application to calculate your age',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/utkarshMAVcode/mern-react-task-5',
+    demo: 'https://mern-react-task-5.vercel.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'word counter',
+    description: `an online application that lets us count the number of words in a paragraph`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/utkarshMAVcode/mern-react-task-1-',
+    demo: 'https://mern-react-task-1.vercel.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'boots starp tasks',
+    description:
+      'my front end tasks i learnt during my mern stack journey',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/utkarshMAVcode/bootstrap-tasks',
+    demo: 'https://bootstrap-tasks-mu.vercel.app/',
+  },
+];
+
+export { services, technologies, experiences, projects };
